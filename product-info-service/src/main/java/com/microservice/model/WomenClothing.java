0818d9ab17a6310c3f8da9e9c67ea59d2186ca70
Priya Gupta <priyagupta14317@gmail.com>
2019-08-11 19:08:45 -0500

    Adding the name of the node which needs to me retrieved

--- product-info-service/src/main/java/com/microservice/model/WomenClothing.java
+++ product-info-service/src/main/java/com/microservice/model/WomenClothing.java
@@ -3,7 +3,7 @@ package com.microservice.model;
 import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
 
-@NodeEntity
+@NodeEntity("Women_Clothing")
 public class WomenClothing {
 	
 	@Id
