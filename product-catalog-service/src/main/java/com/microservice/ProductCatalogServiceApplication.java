8a751f348d1ac501d49ca43de54718ceb2937c8f
Priya Gupta <priyagupta14317@gmail.com>
2019-08-12 00:46:33 -0500

    Adding Neo4jRepositories

--- product-catalog-service/src/main/java/com/microservice/ProductCatalogServiceApplication.java
+++ product-catalog-service/src/main/java/com/microservice/ProductCatalogServiceApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableNeo4jRepositories
 public class ProductCatalogServiceApplication {
 	
 	@Bean
