3372e25d6c0b884bd42f5e240398c83ff2b4ade2
Priya Gupta <priyagupta14317@gmail.com>
2019-08-11 19:54:38 -0500

    Adding annotation

--- discount-info-service/src/main/java/com/microservice/repository/DiscountRepository.java
+++ discount-info-service/src/main/java/com/microservice/repository/DiscountRepository.java
@@ -4,9 +4,10 @@ import java.util.Collection;
 
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.stereotype.Repository;
 
 import com.microservice.model.Discount;
-
+@Repository
 public interface DiscountRepository extends Neo4jRepository<Discount, Long> {
 
 	@Query("MATCH (n:Discount_filter) return n ")
--- discount-info-service/src/main/java/com/microservice/service/DiscountService.java
+++ discount-info-service/src/main/java/com/microservice/service/DiscountService.java
@@ -3,10 +3,11 @@ package com.microservice.service;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.microservice.model.Discount;
 import com.microservice.repository.DiscountRepository;
-
+@Service
 public class DiscountService {
 	
 	@Autowired
